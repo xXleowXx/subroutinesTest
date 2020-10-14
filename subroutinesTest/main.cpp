807313e99e18565f3f51fcbf410c623b2c80d6b9
XleowX <leonard.lipowski@gmail.com>
2020-10-14 11:59:36 +0100

    Add project files.

---
+++ subroutinesTest/main.cpp
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+//prototye
+
+//function procedure and sub routines
+
+int addNumbers(int, int);
+
+
+void main() 
+{
+	std::cout << addNumbers(3, 8) << endl;
+}
+
+int addNumbers(int Number1, int Number2) 
+{
+	return Number1 + Number2;
+}
